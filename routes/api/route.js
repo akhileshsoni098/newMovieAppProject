e6ce8d4f41818e0e214b48c0eab97bcf0bae60d8
Akhilesh Soni <akhileshsoni098@gmail.com>
2023-04-08 00:17:47 +0530

    route.js

--- routes/api/route.js
+++ routes/api/route.js
@@ -7,71 +7,93 @@ const reviewContro = require("../../controllers/revieweController");
 
 const auth = require("../../middi/auth");
 
-// user and admin both
+//=====================================*****user registration to deletion***** ====================
+// ======================== to register as a user or admin ========================================
 router.post("/registration", UserController.userData);
+//===================== log in user/admin =========================================================
 router.post("/logInUser", UserController.logIn);
+// ======================user/admin can see his profile ===========================================
 router.get("/getUser", auth.authentication, UserController.userDetails);
+//=================== user/admin can update in his profile ========================================
 router.put("/updateUser", auth.authentication, UserController.updateUser);
+//================= user/admin can delete his profile =============================================
 router.delete("/deleteUser", auth.authentication, UserController.deleteUser);
-// delete user # have to create
 
-// movie
+//==========================*****movie upload to delete*****=======================================
 
+// =================== only admin can upload movies ===============================================
 router.post("/saveMovie", auth.authentication, movieController.saveMovie);
+
+// ====================== user/admin can get all the movies =======================================
 router.get("/getAllMovies", auth.authentication, movieController.getMovie);
+// ========================= user/admin can use some filter to serach the movies ==================
 router.get("/getFilterMovies", auth.authentication, movieController.queryMovie);
+//================== user can get the movie on movie by movie id  =================================
 router.get(
   "/getById/:movieId",
   auth.authentication,
   movieController.getMovieById
 );
+
+//============================== only that  admin can delete the movie who uploaded the movie =====
 router.delete(
   "/deleteMovie/:movieId",
   auth.authentication,
   movieController.deleteMovie
 );
-
-// watchList
+//==============*****add to watchList to delte from watchList*****=================================
+// ========================user/admin can add movie in the watchList ==============================
 router.post(
   "/addWatchList/:movieId",
   auth.authentication,
   watchlistContro.addToWatchList
 );
+// ============= user/ admin can see all movies with is added in watchList =======================
 router.get(
   "/getWatchList",
   auth.authentication,
   watchlistContro.watchlistDetails
 );
+//================ user/ admin can remove any selected movie from the watchList ===================
 router.delete(
   "/deleteMovieWatchList/:movieId",
   auth.authentication,
   watchlistContro.delParticular
 );
+// ========================user/admin can clear his watchList======================================
 router.delete(
   "/clearAllWatchList",
   auth.authentication,
   watchlistContro.clearWatchList
 );
-
-//review
+//====================*****review creation to deletion*****========================================
+//====================== user/admin can review and rate any selected movie ========================
 router.post("/review/:movieId", auth.authentication, reviewContro.reviewdata);
+//================= user/ admin can get his reviews of any particular movie =======================
 router.get(
   "/getParticular/:movieId",
   auth.authentication,
   reviewContro.getParticular
 );
+//================= user/ admin can get all his reviews of all movie  =============================
 router.get("/getAllReview", auth.authentication, reviewContro.getAllReviews);
 router.put(
   "/reviewUpdate/:movieId",
   auth.authentication,
   reviewContro.updateReview
 );
+//====================== user/admin can delete any particular reviews of any particular movie======
 router.delete(
   "/particularReviewDelete/:movieId",
   auth.authentication,
   reviewContro.DeleteParticular
 );
+//====================== user/admin can delete all reviews of all movie what is given by him =======
 router.delete("/allReviewDelete", auth.authentication, reviewContro.DeleteAll);
+
+
+
+
 
 //test application
 router.get("/", (req, res) => {
